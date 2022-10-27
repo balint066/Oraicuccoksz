99bca4ae8c0e42405ddf58385b4bb01242d98457
balint066 <fazekas.balint.tech2021d@bolyaimovar.com>
2022-10-27 09:24:59 +0200

    alma

--- pythonProject/Gyakorlás_Hőmérséklet.py
+++ pythonProject/Gyakorlás_Hőmérséklet.py
@@ -1,2 +1,26 @@
 # F to C = (F-32)/18
 # C to F = c*1.8+32
+
+homerseklet = float(input("Adja meg az alap hőfokot   "))
+mertekegyseg = input("Mértékegység: f,c,k    ")
+
+if mertekegyseg == "c":
+    F = homerseklet * 1.8 + 32
+    K = homerseklet+273.15
+    print("A hőmérséklet C°-ban", homerseklet)
+    print("A hőmérséklet F°-ben", F)
+    print("A hőmérséklet K°-ben", K)
+
+elif mertekegyseg == "f":
+    C = (homerseklet-32)/1.8
+    K = C+273.15
+    print("A hőmérséklet F°-ben", homerseklet)
+    print("A hőmérséklet C°-ban", C)
+    print("A hőmérséklet K°-ben", K)
+
+elif mertekegyseg == "k":
+    C = homerseklet-273.15
+    F = C*1.8+32
+    print("A hőmérséklet K°-ben", homerseklet)
+    print("A hőmérséklet F°-ben", F)
+    print("A hőmérséklet C°-ban", C)
---
+++ pythonProject/Gyakorlás_homerseklet2.py
@@ -0,0 +1,38 @@
+# F to C = (F-32)/18
+# C to F = c*1.8+32
+
+homerseklet = float(input("Adja meg az alap hőfokot   "))
+mertekegyseg = input("Mértékegység: f,c,k    ")
+valt = input("Váltómértékegység: f,c,k    ")
+if mertekegyseg == "c":
+    if valt == "c":
+        print("A hőmérséklet C°-ban", homerseklet)
+    elif valt == "f":
+        F = homerseklet * 1.8 + 32
+        print("A hőmérséklet F°-ben", F)
+    elif valt == "k":
+        K = homerseklet+273.15
+        print("A hőmérséklet K°-ben", K)
+
+elif mertekegyseg == "f":
+    if valt == "f":
+        print("A hőmérséklet F°-ben", homerseklet)
+    elif valt == "c":
+        C = (homerseklet - 32) / 1.8
+        print("A hőmérséklet C°-ban", C)
+    elif valt == "k":
+        K = ((homerseklet - 32) / 1.8)+273.15
+        print("A hőmérséklet K°-ben", K)
+
+elif mertekegyseg == "k":
+    if valt == "k":
+        print("A hőmérséklet K°-ben", homerseklet)
+    elif valt == "f":
+        F = (homerseklet-273.15) * 1.8 + 32
+        print("A hőmérséklet F°-ben", F)
+    elif valt == "c":
+        C = homerseklet - 273.15
+        print("A hőmérséklet C°-ban", C)
+
+
+
