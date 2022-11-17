80d4440154356ae5ef2de28b69be3d70ac42c4f2
balint066 <fazekas.balint.tech2021d@bolyaimovar.com>
2022-11-17 09:39:55 +0100

    alma

---
+++ pythonProject/magassaglista.py
@@ -0,0 +1,10 @@
+listamagas = []
+osztalyletszam = 3
+for i in range(osztalyletszam):
+    magassag = float(input())
+    listamagas.append(magassag)
+legnagyobb=0
+for i in listamagas:
+    if i>legnagyobb
+        legnagyobb = i
+print(legnagyobb)
