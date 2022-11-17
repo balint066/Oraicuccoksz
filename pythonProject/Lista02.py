0a2cb48eb0f41b41b03ec6761c999ffc78a160ac
balint066 <fazekas.balint.tech2021d@bolyaimovar.com>
2022-11-17 09:35:35 +0100

    alma

--- pythonProject/Lista02.py
+++ pythonProject/Lista02.py
@@ -1,4 +1,16 @@
-l = [1,2,3,4,5]
-
-for i in l:
-    print(i)
+#l = [1,2,3,4,5]
+#l.append(843632636)
+#print(l.index(4))
+#for i in l:
+   # print(i)
+#for i, elem in enumerate(l):
+ #   print(i,elem)
+#felaszn l = []
+#a=0
+#for i in range(5):
+   # a = int(input())
+   # felhaszn.append(a)
+   # if a<0:
+      #  print("kicsi")
+   # else:
+      #  print = a**2
