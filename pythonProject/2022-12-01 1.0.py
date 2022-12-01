21e7e4f33894f05b7e053532e951d5ca20766124
balint066 <fazekas.balint.tech2021d@bolyaimovar.com>
2022-12-01 08:24:26 +0100

    alma

---
+++ pythonProject/2022-12-01 1.0.py
@@ -0,0 +1,24 @@
+#t = [5,3,6,2,1]
+
+#maxElem = t[0]
+#for elem in t:
+#    if elem > maxElem:
+#        maxElem = elem
+#print(maxElem)
+##########################
+#t = [5,3,6,2,1]
+
+#maxElem = t[0]
+#for i in range(1,len(t)):
+#    if t[i] > maxElem:
+#        maxElem = t[i]
+#        maxhely = i
+#print(maxElem,maxhely)
+##########################
+t = [3,8,2,4,5,1,6]
+
+osszeg = 0
+for num in t:
+    osszeg = osszeg + num
+
+print("Összeg:",osszeg)
