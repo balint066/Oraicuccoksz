37aa55c77f2ef949ed556c8c2228655b401545e4
balint066 <fazekas.balint.tech2021d@bolyaimovar.com>
2023-01-26 10:03:38 +0100

    alma

--- pythonProject/Fazekas Bálint 10D dolgozat 2023_01_26.py
+++ pythonProject/Fazekas Bálint 10D dolgozat 2023_01_26.py
@@ -1,6 +1,6 @@
-lista=[10,-2,1,25,22,30,60,-99,-1252,3000,2123,16,12]
+lista = [10, -2, 1, 25, 22, 30, 60, -99, -1252, 3000, 2123, 16, 12]
 
-for i in range(len(lista)-1):
+for i in range(len(lista) - 1):
     minindex = i
 
     for j in range(i + 1, len(lista)):
@@ -12,7 +12,7 @@ for i in range(len(lista)-1):
 print("A növekvő sorrend:  ", lista)
 print("A legkisebb:  ", lista[0])
 
-for i in range(len(lista)-1):
+for i in range(len(lista) - 1):
     minindex = i
 
     for j in range(i + 1, len(lista)):
@@ -22,5 +22,3 @@ for i in range(len(lista)-1):
         lista[i], lista[minindex] = lista[minindex], lista[i]
 print("A csökkenő sorrend:  ", lista)
 print("A legnagyobb:  ", lista[0])
-
-
