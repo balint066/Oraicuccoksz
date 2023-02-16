0064800cdb3f4193c7ffa2d9f8e534c37f3090e7
balint066 <fazekas.balint.tech2021d@bolyaimovar.com>
2023-02-16 09:19:07 +0100

    alma

---
+++ pythonProject/gyakszi 2023.02.16..py
@@ -0,0 +1,19 @@
+a = "qwer"
+for i in a:
+    print(i, end=" ")
+
+
+print(" ")
+
+for j in range(0, len(a), 2):
+    print(a[j], end=" ")
+
+
+print(" ")
+
+for j in a:
+    print(ord(j))
+
+
+print(" ")
+
