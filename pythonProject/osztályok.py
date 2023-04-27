1a596385b3dce12fff4d63e9cbd399dd8cdc9f62
balint066 <fazekas.balint.tech2021d@bolyaimovar.com>
2023-04-27 09:13:28 +0200

    alma

---
+++ pythonProject/osztályok.py
@@ -0,0 +1,7 @@
+import tegla_c
+
+T1 = tegla_c.Teglalap(int(input("a oldal: ")), int(input("b oldal: ")))
+
+
+print(T1.ter(),"a négyzet területe")
+print(T1.ker(),"a négyzet kerülete")
---
+++ pythonProject/tegla_c.py
@@ -0,0 +1,11 @@
+class Teglalap:
+
+    def ter(self):
+        return self.a * self.b
+
+    def ker(self):
+        return 2 * (self.a + self.b)
+
+    def __init__(self, a=0, b=0):
+        self.a = a
+        self.b = b
